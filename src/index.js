08b1f16d918fb0baac3c3965fb8147c1a9e7c28b
Antonio <krixen.dev@gmail.com>
2023-08-23 16:30:02 -0300

    agregar lógica turno usuario
    se agregó un objeto usuario con sus respectivos métodos cuya función es verificar que la secuencia introducida por el usuario sea la mísma que la secuencia de simón, por cada ronda

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ const $cartelAvisoTurno = document.getElementById("aviso-turno");
 const $btnEmpezarJuego = document.getElementById("btn-empezar-juego");
 const $cuadros = document.querySelectorAll("[name=cuadro]");
 
-let secuenciaUsuario = [];
 let ronda = 0;
 
 $btnEmpezarJuego.onclick = empezarJuego;
-
 
 function empezarJuego() {
   mostrarElementoOculto($cartelAvisoTurno);
@@ -15,12 +13,20 @@ function empezarJuego() {
 }
 function gestionarRonda() {
   turnoSimon();
+  setTimeout(() => {
+    turnoUsuario();
+  }, (simon.secuencia.length + 1) * 1000);
+
 }
 function turnoSimon() {
   simon.avisarTurno();
-  simon.agregarCuadroASecuencia(simon.generarCuadroAlAzar());
+  simon.bloquearInputsUsuario();
   simon.agregarCuadroASecuencia(simon.generarCuadroAlAzar());
   simon.mostrarSecuencia();
+}
+function turnoUsuario() {
+  usuario.avisarTurno();
+  usuario.desbloquearInputs();
 }
 
 
@@ -44,8 +50,50 @@ const simon = {
         iluminarCuadro($cuadro)
       }, tiempoRetraso);
     })
+  },
+  bloquearInputsUsuario : function () {
+    $cuadros.forEach($cuadro => {
+      $cuadro.style.cursor = "not-allowed"
+      $cuadro.onclick = function () {
+      }
+    });
   }
 }
+
+const usuario = {
+  secuencia : [],
+  avisarTurno : function () {
+    $cartelAvisoTurno.textContent = "Turno del usuario";
+  },
+  desbloquearInputs : function () {
+    const self = this;
+    $cuadros.forEach(function ($cuadro) {
+      $cuadro.style.cursor = "pointer"
+      $cuadro.onclick = function (e) {
+        self.verificarCoincidencias(e);
+      }
+    });
+  },
+  verificarCoincidencias : function (e) {
+    const $cuadro = e.target;
+    this.secuencia.push($cuadro);
+    iluminarCuadro($cuadro);
+    const $cuadroActualSimon = this.secuencia.length -1;
+    if (this.secuencia.length === simon.secuencia.length) {
+      this.secuencia = [];
+      setTimeout(() => {
+        gestionarRonda();
+      }, 1000);
+    }
+    if ($cuadro.id !== simon.secuencia[$cuadroActualSimon].id) {
+      alert("cuadro equivocado, pierde");
+      return;
+    }
+  }
+}
+
+
+
 
 function iluminarCuadro($cuadro) {
   $cuadro.classList.remove("desactivo");
@@ -55,9 +103,6 @@ function iluminarCuadro($cuadro) {
     $cuadro.classList.add("desactivo")
   }, 500);
 }
-
-
-
 function mostrarElementoOculto($elemento) {
   $elemento.classList.remove("d-none");
 }
